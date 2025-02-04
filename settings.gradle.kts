b6f4949f2ab028eb9cd9b5c019d0f01b248044b6
vibhu <vibhu.rampally@gmail.com>
2025-02-03 19:04:39 -0800

    Added Bar Graph of expenses per month

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,14 +8,14 @@ pluginManagement {
             }
         }
         mavenCentral()
-        gradlePluginPortal()
-    }
+        gradlePluginPortal() }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven("https://jitpack.io")
     }
 }
 
